ff3ac4341b2d9c0e1e13e144f77504caeaedbf6e
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-09-30 22:33:00 +0600

    display image issue fixed

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
+  // console.log(products[0].image);
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.images;
@@ -14,7 +15,7 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
@@ -73,8 +74,8 @@ const updateTaxAndCharge = () => {
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
+      getInputValue("price") + getInputValue("delivery-charge") +
+      getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
 loadProducts();
